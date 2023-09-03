89abf0b3c8ecd4bbf36d20249bacb43588961a8f
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2023-09-04 00:13:27 +0500

    Write the content of the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,30 @@
 import styles from '@/styles/Header.module.css'
+import { useState, useRef } from "react";
+import { useOnClickOutside } from "./useOnClickOutside";
 
 const Header = () => {
+  const [dropdown, setDropdown] = useState(false);
+  const ref = useRef();
+  useOnClickOutside(ref, dropdown, () => setDropdown(false));
+  
   return (
     <header className={styles.header}>
-      <h1>This is Header!</h1>
-      <p>Here are useful links and logo</p>
+      <h1><a>Mountains Forecast</a></h1>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li ref={ref}>
+            <button onClick={() => setDropdown((prev) => !prev)}>
+              Models <span>&#8595;</span>
+            </button>
+            {dropdown && (<ul>
+              <li>ICON</li>
+              <li>GFS</li>
+            </ul>)}
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 };
