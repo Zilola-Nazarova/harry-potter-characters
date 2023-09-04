ecd22be937196dd88f32e80f50f2f32f3b6ff801
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2023-09-04 18:38:53 +0500

    Hardcode the database with mountains names, coordinates and elevations

---
+++ src/database/mountainsData.js
@@ -0,0 +1,22 @@
+const mountainsData = [
+  {
+    name: 'Greater Chimgan',
+    latitude: 41.4951900, 
+    longitude: 70.0586718,
+    elevation: 3309.6,
+  },
+  {
+    name: 'Karakush',
+    latitude: 41.2477890, 
+    longitude: 70.3391467,
+    elevation: 3864.3,
+  },
+  {
+    name: 'Babaitag',
+    latitude: 41.1549650,
+    longitude: 70.2232660,
+    elevation: 3555.7,
+  },
+]
+
+export default mountainsData;
