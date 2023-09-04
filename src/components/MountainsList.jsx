a38b5a0be614137a60a8a5666f939ff5b4d0cbfc
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2023-09-04 18:41:27 +0500

    Display data conditionally in MountainsList component

--- src/components/MountainsList.jsx
+++ src/components/MountainsList.jsx
@@ -1,14 +1,28 @@
-import styles from '@/styles/MountainsList.module.css'
-import { Link } from "react-router-dom"
+import { useSelector, useDispatch } from 'react-redux';
+import styles from '@/styles/MountainsList.module.css';
+import { Link } from "react-router-dom";
+
 const MountainsList = () => {
-  return (
-    <div className={styles.list}>
-      <p>Here is MountainsList</p>
-      <p>See details
-      <Link to={`/details/1`}>Greater Chimgan</Link>
+  const dispatch = useDispatch();
+  const { mountains, isLoading, error } = useSelector((store) => store.mountains);
+
+  if (isLoading) {
+    return <p className={styles.loading}>The forecast is loading!</p>;
+  }
+  if (error) {
+    return (
+      <p className={styles.error}>
+        Something went wrong!
+        <br />
+        { error }
       </p>
-    </div>
-  )
+    );
+  }
+  if (mountains.length !== 0) {
+    return (
+      <p>{JSON.stringify(mountains)}</p>
+    )
+  }
 }
 
 export default MountainsList;
