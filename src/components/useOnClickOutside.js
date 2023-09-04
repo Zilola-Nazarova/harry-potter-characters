3b7523bc97a4f204b211091ee047e488e2ae8937
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2023-09-04 23:20:54 +0500

    Update useOnClickOutside file so that the dropdown closes when a dropdown option is clicked

--- src/components/useOnClickOutside.js
+++ src/components/useOnClickOutside.js
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 export const useOnClickOutside = (ref, currentState, updater) => {
   useEffect(() => {
     const handler = (event) => {
-      if (currentState && ref.current && !ref.current.contains(event.target)) {
+      if (currentState && ref.current) {
         updater();
       }
     };
