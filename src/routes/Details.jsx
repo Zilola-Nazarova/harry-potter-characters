c5694e6218f062013ac633b7fd3f9d30befb4d32
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2023-09-04 21:35:13 +0500

    Provide a prop to Forecast component using useParams

--- src/routes/Details.jsx
+++ src/routes/Details.jsx
@@ -1,8 +1,19 @@
-import ForecastDetails from "@/components/ForecastDetails";
+import Forecast from "@/components/Forecast";
+import { useParams } from 'react-router';
+import { useSelector } from 'react-redux';
 
 const Details = () => {
+  const {mountains} = useSelector((state) => state.mountains);
+  const { id } = useParams();
+  const peak = mountains.find((peak) => peak.name === id);
+  
   return (
-    <ForecastDetails />
+    <>
+      <p>This is forecast</p>
+      <Forecast 
+        peak={peak}
+      />
+    </>
   )
 }
 
