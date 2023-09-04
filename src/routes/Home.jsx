224699d1d526ef5b6ad2240ef9fa4dae8aad7df3
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2023-09-04 18:41:04 +0500

    Dispatch the async function to fetch data prom API when the app loads

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,8 +1,19 @@
-import MountainsList from "@/components/MountainsList";
 import Header from '@/components/Header'
 import { Outlet } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { getCurrentWeather } from "@/redux/mountains/mountainsSlice";
+import { useEffect } from 'react';
+import mountainsData from '@/database/mountainsData';
 
 const Home = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    mountainsData.forEach(
+      (peak) => dispatch(getCurrentWeather(peak))
+    );
+  }, [dispatch]);
+
   return (
     <>
       <Header />
