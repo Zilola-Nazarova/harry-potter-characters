f32818dd2f282c4585e74b3ba7de3f4e3145fda5
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2023-09-06 03:47:48 +0500

    Add more peaks to 'mountainsData' database

--- src/database/mountainsData.js
+++ src/database/mountainsData.js
@@ -39,11 +39,63 @@ const mountainsData = [
     }
   },
   {
-    name: 'Some place',
-    latitude: 45.1549650,
-    longitude: 75.2232660,
-    elevation: 3555.7,
+    name: 'Kizilbash',
+    latitude: 42.0727687,
+    longitude: 70.4667018,
+    elevation: 4200,
     image: 'src/assets/mnt/mnt_04.svg',
+    current_weather: {
+      temperature: null,
+      windspeed: null,
+      windderection: null,
+      time: null,
+    }
+  },
+  {
+    name: 'Tavalgan',
+    latitude: 41.7915524,
+    longitude: 70.4090762,
+    elevation: 3888.8,
+    image: 'src/assets/mnt/mnt_05.svg',
+    current_weather: {
+      temperature: null,
+      windspeed: null,
+      windderection: null,
+      time: null,
+    }
+  },
+  {
+    name: 'Adelunga',
+    latitude: 42.1235559,
+    longitude: 70.9689168,
+    elevation: 4301.8,
+    image: 'src/assets/mnt/mnt_06.svg',
+    current_weather: {
+      temperature: null,
+      windspeed: null,
+      windderection: null,
+      time: null,
+    }
+  },
+  {
+    name: 'Xodja Gur Gur Ota',
+    latitude: 38.3828229,
+    longitude: 67.2788411,
+    elevation: 3723.7,
+    image: 'src/assets/mnt/mnt_07.svg',
+    current_weather: {
+      temperature: null,
+      windspeed: null,
+      windderection: null,
+      time: null,
+    }
+  },
+  {
+    name: 'Hazret Sultan',
+    latitude: 38.9477782,
+    longitude: 68.1724695,
+    elevation: 4643,
+    image: 'src/assets/mnt/mnt_08.svg',
     current_weather: {
       temperature: null,
       windspeed: null,
