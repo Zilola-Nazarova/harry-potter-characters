ed0408a076c877c7fac35eff16d287aa33ae61d1
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2023-09-05 06:29:35 +0500

    Include all keys to database to avoid errors when the data is loading

--- src/database/mountainsData.js
+++ src/database/mountainsData.js
@@ -4,18 +4,48 @@ const mountainsData = [
     latitude: 41.4951900, 
     longitude: 70.0586718,
     elevation: 3309.6,
+    current_weather: {
+      temperature: null,
+      windspeed: null,
+      windderection: null,
+      time: null,
+    }
   },
   {
     name: 'Karakush',
     latitude: 41.2477890, 
     longitude: 70.3391467,
     elevation: 3864.3,
+    current_weather: {
+      temperature: null,
+      windspeed: null,
+      windderection: null,
+      time: null,
+    }
   },
   {
     name: 'Babaitag',
     latitude: 41.1549650,
     longitude: 70.2232660,
     elevation: 3555.7,
+    current_weather: {
+      temperature: null,
+      windspeed: null,
+      windderection: null,
+      time: null,
+    }
+  },
+  {
+    name: 'Some place',
+    latitude: 41.1549650,
+    longitude: 75.2232660,
+    elevation: 3555.7,
+    current_weather: {
+      temperature: null,
+      windspeed: null,
+      windderection: null,
+      time: null,
+    }
   },
 ]
 
