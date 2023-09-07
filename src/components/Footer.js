df656712d37e0edd262cf1adfcf9bb0f7f9aaa6f
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2023-09-08 01:18:13 +0500

    Rewrite and restyle About component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,16 +3,7 @@ import styles from '../styles/Footer.module.css';
 
 const Footer = () => (
   <div className={styles.footer}>
-    <p>
-      The weather forecast and all info about spots is provided for
-      personal non-commercial use.
-    </p>
-    <p>
-      Mountains Forecast does not promise any specific results from
-      the use of its service.
-    </p>
-    <p>© 2023 Mountains Forecast</p>
-
+    <p>© 2023 Harry Potter Library</p>
     <div className={styles.contacts}>
       <p>
         <a href="#/">Send a message</a>
--- src/routes/About.js
+++ src/routes/About.js
@@ -2,70 +2,41 @@ import styles from '../styles/About.module.css';
 
 const About = () => (
   <div className={styles.about}>
-    <h2 className={styles.bright}>What is Mountains Forecast?</h2>
+    <h2 className={styles.bright}>What is Harry Potter Lybrary?</h2>
     <p>
-      Mountains Forecast is a site that lists down the mountains
-      located in Uzbekistan with the following data for each
-      mountain:
+      Harry Potter Lybrary is a site that lists down main Harry Potter
+      characters with the following data:
     </p>
     <ul className={styles.dark}>
-      <li>elevation (in m)</li>
-      <li>weather code (index)</li>
+      <li>Name</li>
+      <li>House</li>
+      <li>Alive/Not alive</li>
+      <li>Character&apos;s portrait</li>
     </ul>
     <p>
-      When a mountain is selected, current weather
-      and hourly forecasts are displayed:
+      When a character is selected, more data about this character
+      is displayed such as:
     </p>
     <ul>
-      <li className={styles.bright}>
-        <span>current weather:</span>
-        <ul>
-          <li>temperature (in °C)</li>
-          <li>wind speed (in km/h)</li>
-          <li>wind direction (in °)</li>
-        </ul>
-      </li>
-      <li className={styles.bright}>
-        <span>hourly forecast:</span>
-        <ul>
-          <li>temperature (in °C)</li>
-          <li>precipitation (in mm)</li>
-          <li>wind speed (in km/h)</li>
-          <li>cloudcover (in %)</li>
-        </ul>
-      </li>
+      <li>Wand properties</li>
+      <li>Patronus animal</li>
+      <li>Date of birth</li>
+      <li>Ancestry</li>
     </ul>
     <p>
-      You can swith the displayed forecast to show information
-      according to ICON or GFS forecast models.
-    </p>
-    <p>
-      Every page containing the weather data displays
-      information about the last update under the navigation
-      bar.
-    </p>
-    <p>
-      The navigation bar has Models button with links to
-      information about main forecast models: ICON and GFS.
+      The home page has a search bar where you can type your
+      character&apos;s name to find the information faster.
     </p>
 
     <div className={styles.credits}>
       <h3 className={styles.bright}>thanks</h3>
       <p>
-        Weather information by
+        Harry Potter characters information by
         {' '}
-        <a target="_blank" rel="noreferrer" href="https://open-meteo.com/">
-          open-meteo.com
+        <a target="_blank" rel="noreferrer" href="https://hp-api.onrender.com/">
+          hp-api.onrender.com/
         </a>
         API.
-      </p>
-      <p>
-        Map of Uzbekistan by
-        {' '}
-        <a target="_blank" rel="noreferrer" href="https://vemaps.com/">
-          Vemaps.com
-        </a>
-        .
       </p>
       <p>
         Original design idea by
