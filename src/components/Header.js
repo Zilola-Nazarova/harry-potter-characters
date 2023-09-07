3509a6da0958c79492b1d3f393470a78597f6f32
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2023-09-08 01:25:15 +0500

    Delete unnecessary pieces of code (since I changed the topic)

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,43 +1,36 @@
-import { useState, useRef } from 'react';
 import { NavLink } from 'react-router-dom';
 import { IoIosArrowBack } from 'react-icons/io';
 import styles from '../styles/Header.module.css';
-import useOnClickOutside from './useOnClickOutside';
 
-const Header = () => {
-  const [dropdown, setDropdown] = useState(false);
-  const ref = useRef();
-  useOnClickOutside(ref, dropdown, () => setDropdown(false));
-  return (
-    <header className={styles.header}>
-      <nav className={styles.navigation}>
-        <ul>
-          <li className={styles.arrow}>
-            <NavLink to="/">
-              <IoIosArrowBack />
-            </NavLink>
-          </li>
-          <li className={styles.title}>
-            <NavLink to="/">
-              <h1>
-                home
-              </h1>
-            </NavLink>
-          </li>
-          <li>
-            <NavLink data-testid="clickAbout" to="/about">
-              about
-            </NavLink>
-          </li>
-          <li ref={ref}>
-            <NavLink to="/models">
-              models
-            </NavLink>
-          </li>
-        </ul>
-      </nav>
-    </header>
-  );
-};
+const Header = () => (
+  <header className={styles.header}>
+    <nav className={styles.navigation}>
+      <ul>
+        <li className={styles.arrow}>
+          <NavLink to="/">
+            <IoIosArrowBack />
+          </NavLink>
+        </li>
+        <li className={styles.title}>
+          <NavLink to="/">
+            <h1>
+              home
+            </h1>
+          </NavLink>
+        </li>
+        <li>
+          <NavLink data-testid="clickAbout" to="/about">
+            about
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/models">
+            models
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  </header>
+);
 
 export default Header;
