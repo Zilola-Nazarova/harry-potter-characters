4bea6c9ff5b14747cb318441dfbc3e861c273739
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2023-12-12 14:28:46 +0500

    Remove script and 404.html, remove basename

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import './index.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="/harry-potter-characters">
+      <BrowserRouter>
         <Root />
       </BrowserRouter>
     </Provider>
